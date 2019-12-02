cf14fb2b4d7f9d51ebb026bde42ace1bbd12a463
Puchaco <jc15@live.com.mx>
2019-12-02 00:17:07 -0600

    Tabla usuarios con el insert en el DAO
    En teoría ya está terminada la tabla de usuarios y ya hay una query para insertarlos

--- app/src/main/java/com/fsd/proyectoedson10/DB/DAO/UsersDAO.kt
+++ app/src/main/java/com/fsd/proyectoedson10/DB/DAO/UsersDAO.kt
@@ -1,4 +1,15 @@
 package com.fsd.proyectoedson10.DB.DAO
 
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+
 interface UsersDAO {
+
+    @Insert
+    fun insertUser(user : UsersDAO)
 }
