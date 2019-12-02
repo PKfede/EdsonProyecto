849a348c11376ce1e0f2b730f6381d01f77c5456
PKfede <47314404+pkfede@users.noreply.github.com>
2019-12-02 00:44:24 -0600

    se me fue una s en lists en un campo

--- app/src/main/java/com/fsd/proyectoedson10/DB/Entities/ListsETY.kt
+++ app/src/main/java/com/fsd/proyectoedson10/DB/Entities/ListsETY.kt
@@ -14,7 +14,7 @@ import androidx.room.*
 data class  ListsETY(@ColumnInfo(name="userId") var userId: String){
 
     @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "idLista") var idLista: String = ""
-    @field:ColumnInfo(name = "listName") var listsName: String = ""
+    @field:ColumnInfo(name = "listName") var listName: String = ""
     @field:ColumnInfo(name = "listColor") var listColor: String = ""
     @field:ColumnInfo(name = "listIcon") var listIcon: String = ""
 
