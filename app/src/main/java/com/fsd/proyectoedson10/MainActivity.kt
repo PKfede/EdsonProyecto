4e1baad5811d180198b003b7be9e433ed0567dfe
Alejandro Brito <47316052+Brito9539@users.noreply.github.com>
2019-12-05 13:18:14 -0600

    DemosAdapters in class

--- app/src/main/java/com/fsd/proyectoedson10/MainActivity.kt
+++ app/src/main/java/com/fsd/proyectoedson10/MainActivity.kt
@@ -35,58 +35,6 @@ import com.fsd.proyectoedson10.ui.task.TaskFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_list.*
 
-class DemoAdapter(private val tasks: Array<TaskETY>) :
-    RecyclerView.Adapter<DemoAdapter.DemoViewHolder>() {
-
-    class DemoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private var tvName: TextView
-        private var imagePriority : ImageView
-        private var creator : TextView
-        private var date : TextView
-        private var description : TextView
-
-        init {
-            tvName = view.findViewById(R.id.name)
-            imagePriority = view.findViewById(R.id.iconPriority)
-            creator = view.findViewById(R.id.creator)
-            date = view.findViewById(R.id.date)
-            description = view.findViewById(R.id.description)
-
-        }
-
-        val db = AppDatabase.getAppDatabase(view.context)
-
-        public fun bind(task: TaskETY) {
-            tvName.setText(task.title)
-            when(task.priority){
-                "0" -> {imagePriority.setImageResource(R.drawable.sin_importancia)}
-                "1" -> {imagePriority.setImageResource(R.drawable.baja)}
-                "2" -> {imagePriority.setImageResource(R.drawable.normal)}
-                "3" -> {imagePriority.setImageResource(R.drawable.alta)}
-            }
-            creator.setText("${db.UserDAO().getUser().lastName}, ${db.UserDAO().getUser().name}" )
-            date.setText(task.expiredDate)
-            description.setText(task.description)
-        }
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): DemoAdapter.DemoViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.rv_demo_holder, parent, false) as View
-
-        return DemoViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: DemoViewHolder, position: Int) {
-        holder.bind(tasks[position])
-    }
-
-    override fun getItemCount() = tasks.size
-}
-
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -130,7 +78,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             finish()
         }
 
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         AppDatabase.setNav(navView)
@@ -152,7 +99,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(menu: MenuItem): Boolean {
-
 
         menu.isChecked = true
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
