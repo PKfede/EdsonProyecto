99e46619f343ec2a8b4c9202a5a711856fb4cdd0
Alejandro Brito <47316052+Brito9539@users.noreply.github.com>
2019-12-05 17:41:03 -0600

    DemoAdapter

--- app/src/main/java/com/fsd/proyectoedson10/DB/sharedlist/SharedListFragment.kt
+++ app/src/main/java/com/fsd/proyectoedson10/DB/sharedlist/SharedListFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fsd.proyectoedson10.DemoAdapterTaskNotification
 
 import com.fsd.proyectoedson10.R
 
@@ -22,7 +25,14 @@ class SharedListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view =  inflater.inflate(R.layout.fragment_shared_list, container, false)
-        
+
+
+
+        var rv = view.findViewById<RecyclerView>(R.id.rv3).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@SharedListFragment.context)
+            //adapter = DemoAdapterTaskNotification()
+        }
         return view
     }
 
